4d700c8bcd3fb22fca3dfaf8e365c0495649e031
unknown <L00144762@student.lyit.ie>
2019-04-10 16:00:16 +0100

    second commit - section 1 edit to NA values

--- CA2.R
+++ CA2.R
@@ -22,7 +22,7 @@ colnames(NI_Postcode) <- c("Organisation Name","Sub-Building Name", "Building Na
 
 # pasting colums together; tidying up dataframe
 NI_Pcode <- NI_Postcode
-NI_Pcode$`Alt/Secondary Thorfare` <- paste(NI_Pcode$`Alt Thorfare`, NI_Pcode$`Secondary Thorfare`)
+NI_Pcode$`Secondary Thorfare` <- paste(NI_Pcode$`Alt Thorfare`, NI_Pcode$`Secondary Thorfare`)
 
 
 NI_Pcode$`x/y coordinates` <- paste(NI_Pcode$`x-coordinates`, ",", NI_Pcode$`y-coordinates`)
@@ -31,7 +31,7 @@ NI_Pcode$`x/y coordinates` <- paste(NI_Pcode$`x-coordinates`, ",", NI_Pcode$`y-c
 
 
 # reordering the dataset
-NI_Pcode= NI_Pcode %>% select (`Primary Key (identifier)`,`Organisation Name`, `Sub-Building Name`
+NI_Pcode= NI_Pcode %>% select(`Primary Key (identifier)`,`Organisation Name`, `Sub-Building Name`
                                        , `Building Name`, Number, `Primary Thorfare`, `Alt/Secondary Thorfare`
                                        ,Locality,Townland, Town, County, Postcode, `x/y coordinates`)
 
@@ -42,15 +42,29 @@ NI_Pcode[NI_Pcode == "NA,NA"] <- NA   #any cells containg NA, NA
 NI_Pcode[NI_Pcode == ","] <- NA       #cells containg jsut the seperator are replaced
 NI_Pcode[NI_Pcode == " "] <- NA      #cells with a space seperator are replaced
 
-head(NI_Pcode)
+
+# replacing NA values in Locality with value in Townland colum; makes most logical sense
+# same for secondary thorfare with primary thorfare values
+#NA values in organisation name field replaced with "RESIDENT"
+#Sub-building and building left as is
+NI_Pcode$Locality <- ifelse(is.na(NI_Pcode$Locality), NI_Pcode$Townland, NI_Pcode$Locality)
+
+NI_Pcode$`Secondary Thorfare` <- ifelse(is.na(NI_Pcode$`Alt/Secondary Thorfare`), NI_Pcode$`Primary Thorfare`
+                                            , NI_Pcode$`Alt/Secondary Thorfare`)
+
+NI_Pcode$`Organisation Name` <- ifelse(is.na(NI_Pcode$`Organisation Name`), "RESIDENT", 
+                                       NI_Pcode$`Organisation Name`)
 
 
 # checking what number of rows contain NA values compared to the total number of rows in dataframe
 nrow(NI_Pcode) 
 sum(!complete.cases(NI_Pcode)) 
 
-#results: every row contains at least 1 NA value so removing all the NA values will ruin the dataset
-# a few columns were pasted together to tidy up the dataset
+#results: every row (apart from 6) contains at least 1 NA value 
+#removing all the NA values will result in scarce data
+
+
+complete_rows <- na.omit(NI_Pcode)
 
 # (d) (i) total NA values per col in dataset
 NA_per_col <- colSums(is.na(NI_Pcode))
@@ -69,8 +83,10 @@ levels(NI_Pcode$County)
 
 # making limavady dataset
 limavady <- subset(NI_Pcode,  Town == "LIMAVADY", select =`Locality`:`Town`)
+limavady$Locality <- ifelse(is.na(limavady$Locality), limavady$Townland, limavady$Locality)
 
-
-
+limavady$Locality <- as.factor(limavady$Locality)
+str(limavady)
+write.csv(limavady, 'C:\\Users\\Richard\\Desktop\\data_science\\CleanNIPostcodeData.csv', row.names = FALSE)
 
 
